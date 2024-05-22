bea60262d7d4b640d2e80ec5318e30e90f80abec
kawsar121 <tohidulislamkawsarbhuiyan2567gmail.com>
2024-05-22 10:06:20 +0600

    first commit

---
+++ first.js
@@ -0,0 +1,41 @@
+                // Javasript Practis Time
+
+
+const obj = {
+    name : 'kawsar',
+    roll : 683480
+}
+// console.log(obj.roll)
+// document.write(obj)
+
+
+// Addition
+
+let firstNum = 5;
+let lastNum = 5;
+let add = firstNum + lastNum;
+console.log(add)
+
+
+// Subtruction
+
+let firstNum2 = 5;
+let lastNum2 = 5;
+let sub = firstNum2 - lastNum2;
+console.log(sub)
+
+
+// Multification
+
+let mulFirst = 5;
+let mulSecond = 5;
+let mulTification = mulFirst * mulSecond;
+console.log(mulTification)
+
+
+// Divission
+
+let divFirst = 5;
+let divSecond = 5;
+let divission = divFirst / divSecond;
+console.log(divission)
