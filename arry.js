331bd0699799b3c658ab79215f869d90cc5bae95
kawsar121 <tohidulislamkawsarbhuiyan2567gmail.com>
2024-05-24 12:29:50 +0600

    arry complete succecfully

---
+++ arry.js
@@ -0,0 +1,28 @@
+/*
+
+* push er kaz holo shobar sheshe akta element jog kora;
+* pop er kaz holo shobar sheshe element bad dewa ,, shekhetre evave dile hobe pop();
+* unshift er kaz holo pothom dike element jog jora;
+* shift er kaz holo pothom dike element bad dewa;
+
+            Example::
+
+const arry = ['kamal' , 'jamal' , 'lamal']
+
+Ex.1: arry.push('rahim','abdul')     
+Ex.2: arry.pop()    #pop e kisu dewa lage na karon she nijei remove kore dew
+Ex.3: arry.unshift('lal miya')
+Ex.3: arry.shift()  #pop e kisu dewa lage na karon she nijei remove kore dew
+console.log(arry)
+
+
+
+
+*/
+
+
+
+
+
+
+
